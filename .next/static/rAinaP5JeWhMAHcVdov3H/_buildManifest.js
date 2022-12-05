bb3ee9b01978a753778a84ea34cfb0e800aef1f5
tuanln1201 <tuanln.dev@gmail.com>
2022-12-05 15:16:22 +0700

    Update node

--- .next/static/rAinaP5JeWhMAHcVdov3H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6cd5d3474c8b566.css","static/chunks/pages/index-f7963a05f029bf09.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
